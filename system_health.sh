223f906a1ba62df37388b9a895c93a32f7c0f339
Adetola <adetola442@gmail.com>
2025-09-09 01:23:23 +0000

    Modified system_health.sh

--- system_health.sh
+++ system_health.sh
@@ -6,7 +6,15 @@
 CPU_THRESHOLD=80
 MEM_THRESHOLD=80
 DISK_THRESHOLD=90
+#
+#
+# Log file
+LOG_FILE="./system_health.log"
+touch "$LOG_FILE"
 
+log () {
+	echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >> $LOG_FILE
+}
 
 # 1. CPU Check
 cpu_usage=$( top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}' )
@@ -15,10 +23,9 @@ idle=$( top -bn1 | grep "Cpu(s)" | awk '{ print $8 }' )
 
 if (( $(echo "$cpu_usage > $CPU_THRESHOLD" | bc -l) ))
 then
-	echo "cpu usage is high:  $cpu_usage% (Threshold: {$CPU_THRESHOLD})"
+	log "cpu usage is high:  $cpu_usage% (Threshold: $CPU_THRESHOLD%)"
 else
-	echo The CPU usage: $cpu_usage%
-	echo The CPU idle: $idle%	
+	log "The CPU usage: $cpu_usage% |  idle: $idle%"
 fi
 
 
@@ -29,9 +36,9 @@ mem_usage=$( echo "scale=2; $used/$total*100" | bc )
 
 if (( $( echo "$mem_usage > $MEM_THRESHOLD" | bc -l) ))
 then
-	echo "mem usage is high:  $mem_usage% (Threshold: {$MEM_THRESHOLD})"
+	log "mem usage is high:  $mem_usage% (Threshold: $MEM_THRESHOLD%)"
 else
-	echo mem usage is normal: $mem_usage%
+	log "mem usage is normal: $mem_usage%"
 fi
 
 
@@ -43,32 +50,37 @@ do
 	
 	if [ $usage -gt $DISK_THRESHOLD ]
 	then
-		echo "Disk usage is high: $filename($usage%)"
+		log "Disk usage is high: $filename($usage%)"
 	else
-		echo "Disk usage is normal: $filename($usage%)"
+		log "Disk usage is normal: $filename($usage%)"
 	fi
 done
 
 
+
+# 4. Running Processes
 total_high_usage=0
-count=0
-# 4. Running Processes.
-ps aux | awk 'NR>1 {print $2 " " $3 " " $4 " " $11}' | while read -r PID CPU MEM COMMAND
+while read -r PID CPU MEM COMMAND
 do
-	count=$(( count + 1 ))
 	if (( $( echo "$MEM > $MEM_THRESHOLD || $CPU > $CPU_THRESHOLD" | bc -l) ))
 	then
 		(( total_high_usage++ ))
-		echo "High CPU/MEM Usage Detected
-			Process: $PID | CPU: $CPU% | MEM: $MEM%	| COMMAND: $COMMAND
-		"
+		log "High usage Process: $PID | CPU: $CPU% | MEM: $MEM%	| COMMAND: $COMMAND"
 	fi
-done
+done < <(ps aux | awk 'NR>1 {print $2 " " $3 " " $4 " " $11}')
 
-echo "The total high usage processes: $total_high_usage"
-echo "$count"
+log "==================================================================================="
+log "The total high usage processes: $total_high_usage"
+log "==================================================================================="
 
 
+# Exit code
+if [ $total_high_usage -gt 0 ]
+then
+	exit 1
+else
+	exit 0
+fi
 
 
 
